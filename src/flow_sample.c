cf3e8e337a08b2bf61897aa14123f97c35d4e856
Víctor <victor.ballester.ribo@gmail.com>
2023-06-17 18:35:28 +0200

    corrected numMax vs np

--- src/flow_sample.c
+++ src/flow_sample.c
@@ -7,12 +7,12 @@
 
 int main(int argc, char const *argv[]) {
   double hmin = 0.01, hmax = 0.05, tol = 0.000001;
-  int np = 100, n = 1;
+  int numMax = 100, n = 1;
   double x[n], t, t0, h = 0.01, T = 1;
 
   t = t0 = 1;
   x[0] = 1;
-  if (!flow(&t, x, &h, T, hmin, hmax, tol, np, n, ivp2, NULL))
+  if (!flow(&t, x, &h, T, hmin, hmax, tol, numMax, n, ivp2, NULL))
     printf("tolerance: %lf\n(t + T) ^ 2:\n\tReal:  %.10lf\n\tAprox: %.10lf\n\tError: %g\n\n", tol, (t0 + T) * (t0 + T), x[0], fabs((t0 + T) * (t0 + T) - x[0]));
   else
     printf("Error in the numerical computation.\n");
@@ -21,7 +21,7 @@ int main(int argc, char const *argv[]) {
   t = t0 = 0;
   x[0] = 1;
   x[1] = 1;
-  if (!flow(&t, x, &h, T, hmin, hmax, tol, np, n, harmonicOscillator, NULL))
+  if (!flow(&t, x, &h, T, hmin, hmax, tol, numMax, n, harmonicOscillator, NULL))
     // printf("tolerance: % lf\ncos(t + T) + sin(t + T):\n\tReal:  %.10lf\n\tAprox: %.10lf\n\tError: %g\n", tol, cos(t0 + T) + sin(t0 + T), x[0], fabs(cos(t0 + T) + sin(t0 + T) - x[0]));
     printf("tolerance: %lf\ncos(t + T) + sin(t + T):\n\tReal:  %.10lf\n\tAprox: %.10lf\n\tError: %g\n", tol, cos(t0 + T) + sin(t0 + T), x[0], fabs(cos(t0 + T) + sin(t0 + T) - x[0]));
   else
